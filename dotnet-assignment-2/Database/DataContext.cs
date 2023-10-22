78864bfd187d8eddd35f3b0f12b7ee5ab648b68d
fadi-gorges <Fadi.Gorges@student.uts.edu.au>
2023-10-22 22:35:36 +1100

    feat: Added SQLite integration with Entity Framework Core

---
+++ dotnet-assignment-2/Database/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using dotnet_assignment_2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_assignment_2.Database
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("FileName=database.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<SubCategory> SubCategories { get; set; } = null!;
+        public DbSet<Transaction> Transactions { get; set; } = null!;
+    }
+}
