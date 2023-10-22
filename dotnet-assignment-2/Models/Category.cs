f8d8597ff9c265edb7bd7a040c9b229d4d140341
fadi-gorges <Fadi.Gorges@student.uts.edu.au>
2023-10-22 22:30:51 +1100

    feat: Added model classes

---
+++ dotnet-assignment-2/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_assignment_2.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public List<SubCategory> SubCategories { get; set; } = null!;
+    }
+}
---
+++ dotnet-assignment-2/Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿namespace dotnet_assignment_2.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public decimal Nominal { get; set; }
+        public DateTime Date { get; set; }
+        public string Notes { get; set; } = string.Empty;
+        public TransactionType TransactionType { get; set; }
+
+        public int UserId { get; set; }
+        public int SubCategoryId { get; set; }
+
+        public User User { get; set; } = null!;
+        public SubCategory SubCategory { get; set; } = null!;
+    }
+
+    public enum TransactionType
+    {
+        Income,
+        Expense
+    }
+}
